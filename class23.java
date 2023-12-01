afffe258ad356f41d42fc94c4dff12e7db09d28b
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-12-01 11:49:46 +0800

    Create class23.java

---
+++ class23.java
@@ -0,0 +1,7 @@
+public class class23 {
+    public static void main(String[] args) {
+        String stringValue = Double.toString(67.834);
+
+        System.out.println(stringValue.length());
+    }
+}
