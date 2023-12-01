c6c25ff9d6d67e45f72e4db15eb9508821328066
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-12-01 11:49:17 +0800

    Create class22.java

---
+++ class22.java
@@ -0,0 +1,7 @@
+public class class22 {
+    public static void main(String[] args) {
+        double result = Math.sin(Math.PI / 6) + Math.cos(Math.PI / 3);
+
+        System.out.println(result);
+    }
+}
