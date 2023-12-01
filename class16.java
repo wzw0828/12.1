5d8fb31b4b61f44c6d2a48dce63ad25abc8a5bad
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-12-01 11:22:52 +0800

    Create class16.java

---
+++ class16.java
@@ -0,0 +1,9 @@
+public class class16 {
+    public static void main(String[] args) {
+        String str = "262904713";
+
+        int int1 = Integer.parseInt(str);
+        System.out.println(int1 * 2);
+    }
+    
+}
