42fb1f3463b0db5e1c124fa1092c05783939472c
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-12-01 11:47:45 +0800

    Update class18.java

--- class18.java
+++ class18.java
@@ -1,9 +1,8 @@
 public class class18 {
     public static void main(String[] args) {
         String myString = "45874356676541331";
-
         long myLong = Long.parseLong(myString);
-
+        
         System.out.println(myLong);
     }
 }
