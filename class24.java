5f8fb891e28669ee2be2654376ce0fbaa8f87a75
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-12-01 11:50:32 +0800

    Create class24.java

---
+++ class24.java
@@ -0,0 +1,7 @@
+public class class24 {
+    public static void main(String[] args) {
+        String str = "Two heads are better than one.";
+
+        System.out.println(str.substring(14,20));
+    }
+}
