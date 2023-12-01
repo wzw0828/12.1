61abf87a698307ed8959818987d031147f9827ca
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-12-01 10:46:48 +0800

    Delete Ex07 directory

--- Ex07/aaa/Ex12_7.java
+++
@@ -1,9 +0,0 @@
-package L12.Ex07.aaa;
-import L12.Ex07.bbb.Average;
-
-public class Ex12_7 {
-    public static void main(String[] args) {
-        Average a1 = new Average(10);
-        System.out.println("avg="+a1.averg());
-    }
-}
