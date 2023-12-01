9a600e67a7f190072df2dddd1495a5c31fb9fca5
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-12-01 11:47:25 +0800

    Create class19.java

---
+++ class19.java
@@ -0,0 +1,10 @@
+public class class19 {
+    public static void main(String[] args) {
+        int n = 5;
+        double sum = 0.0;
+        for (int i = 1; i <= n; i++) {
+            sum +=Math.sqrt(i);
+        }
+        System.out.println(sum);
+    }
+}
