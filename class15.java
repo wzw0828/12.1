1c748dfad5e0f3e595ac0b256d6ba8190ed92966
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-12-01 11:21:26 +0800

    Create class15.java

---
+++ class15.java
@@ -0,0 +1,7 @@
+public class class15 {
+    public static void main(String[] args) {
+        double number1 = 123.45;
+        String string1 = Double.toString(number1);
+        System.out.println("String1: " + string1.length());
+    }
+}
