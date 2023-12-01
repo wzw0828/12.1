fb7852ca313c0fff7d197998b45af3f67fd92889
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-12-01 11:24:35 +0800

    Create class18.java

---
+++ class18.java
@@ -0,0 +1,9 @@
+public class class18 {
+    public static void main(String[] args) {
+        String myString = "45874356676541331";
+
+        long myLong = Long.parseLong(myString);
+
+        System.out.println(myLong);
+    }
+}
